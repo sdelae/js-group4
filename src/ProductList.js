1c61ac45693724488219cb3dd16cac57f805ca3a
sdelae <151040920+sdelae@users.noreply.github.com>
2024-03-11 17:24:47 +0100

    fix

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,3 +1,4 @@
+// ProductList.js
 import React from 'react';
 
 function ProductList({ addToCart, inventory, selectedSizes, handleSizeChange, handleAddToCart }) {
@@ -8,31 +9,33 @@ function ProductList({ addToCart, inventory, selectedSizes, handleSizeChange, ha
           <img src={product.image} alt={product.name} />
           <h3>{product.name}</h3>
           <p>${product.price.toFixed(2)}</p>
-          {product.category === 'sweaters' || product.category === 'shirts' ? (
-            <div>
-              <select 
-                value={selectedSizes[product.id] || ''} 
-                onChange={(e) => handleSizeChange(product.id, e.target.value)}
-                className="dropdown size"
-              >
-                <option value="">Select size</option>
-                <option value="Small">Small</option>
-                <option value="Medium">Medium</option>
-                <option value="Large">Large</option>
-              </select>
-              <button 
-                onClick={() => selectedSizes[product.id] 
-                  ? addToCart({ ...product, size: selectedSizes[product.id] }) 
-                  : alert('Please select a size')}
-              >
+          <div className="product-actions">
+            {product.category === 'sweaters' || product.category === 'shirts' ? (
+              <>
+                <select 
+                  value={selectedSizes[product.id] || ''} 
+                  onChange={(e) => handleSizeChange(product.id, e.target.value)}
+                  className="dropdown size"
+                >
+                  <option value="">Select size</option>
+                  <option value="Small">Small</option>
+                  <option value="Medium">Medium</option>
+                  <option value="Large">Large</option>
+                </select>
+                <button 
+                  onClick={() => selectedSizes[product.id] 
+                    ? addToCart({ ...product, size: selectedSizes[product.id] }) 
+                    : alert('Please select a size')}
+                >
+                  Add to Cart
+                </button>
+              </>
+            ) : (
+              <button onClick={() => addToCart(product)}> 
                 Add to Cart
               </button>
-            </div>
-          ) : (
-            <button onClick={() => addToCart(product)}> 
-            Add to Cart
-          </button>
-          )}
+            )}
+          </div>
         </div>
       ))}
     </div>
